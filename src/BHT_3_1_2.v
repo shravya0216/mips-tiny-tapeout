3bfa947a0e822f39542d85802fc6fda171e7fc03
shravya0216 <f20230216@goa.bits-pilani.ac.in>
2026-09-18 12:58:59 +0530

    Fix BHT synthesis representation

--- src/BHT_3_1_2.v
+++ src/BHT_3_1_2.v
@@ -16,32 +16,34 @@
 // Revision:
 // Revision 0.01 - File Created
 // Additional Comments:
-// 
+//
 //////////////////////////////////////////////////////////////////////////////////
 
-
 module BHT(
-input clk,
-input [3:0] rd_addr,//this will be connected to XOR_result or BHT_wr_addr
-input BHT_write_control,//this is connecetd to output of branching unit 
-input [3:0] ID_EX_BHT_wr_addr,//this is connected to the XOR result  that is in execution unit flowed from instruction fetch stage or in simple terms ID/EX.BHT_rd_addr
-input BHT_write_data,//this is connected to the zero status of the ALU from execution unit 
-input rst,
-output  BHT_rd_data //now this is an input to the mux 1 it is the select line 
-    );
-    integer i;
-    reg BHT [15:0];
+    input clk,
+    input [3:0] rd_addr,              // this will be connected to XOR_result or BHT_wr_addr
+    input BHT_write_control,          // this is connected to output of branching unit
+    input [3:0] ID_EX_BHT_wr_addr,    // this is connected to the XOR result
+    input BHT_write_data,              // this is connected to the zero status of the ALU
+    input rst,
+    output BHT_rd_data                 // select line for mux 1
+);
+
+    // 16 one-bit BHT entries represented as a packed vector
+    reg [15:0] BHT;
+
     assign BHT_rd_data = BHT[rd_addr];
+
     always @ (posedge clk or posedge rst)
     begin
-    if(rst)
-    begin
-    for(i=0;i<=15;i=i+1)
-    BHT[i]<=1'b0;
+        if (rst)
+        begin
+            BHT <= 16'b0;
+        end
+        else if (BHT_write_control)
+        begin
+            BHT[ID_EX_BHT_wr_addr] <= BHT_write_data;
+        end
     end
-    else if (BHT_write_control)
-    begin
-    BHT[ID_EX_BHT_wr_addr]<=BHT_write_data;
-    end 
-    end
+
 endmodule
